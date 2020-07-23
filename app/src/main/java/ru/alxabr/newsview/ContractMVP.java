547c733cecefbffff781cc32b7828b3e2ee6fc82
Alexandr Abramenko <alx.abr@bk.ru>
2020-07-23 18:13:32 +0700

    The interfaces for the presenter, model and view have been written.

--- app/src/main/java/ru/alxabr/newsview/ContractMVP.java
+++ app/src/main/java/ru/alxabr/newsview/ContractMVP.java
@@ -1,4 +1,25 @@
 package ru.alxabr.newsview;
 
+import java.util.ArrayList;
+
+import ru.alxabr.newsview.Model.Wrapper.News;
+
 public interface ContractMVP {
+    interface View {
+        void disableUI();
+        void enableUI();
+        void showBigLoad();
+        void hideBigLoad();
+        void showError();
+        void updateNewsList();
+    }
+
+    interface Presenter {
+        void showNewsList();
+        void updateNewsList();
+    }
+
+    interface Model {
+        ArrayList<News> readRss();
+    }
 }
