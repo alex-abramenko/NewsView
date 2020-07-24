4633487c7d8d89723267c5d3f155b69aea966001
Alexandr Abramenko <alx.abr@bk.ru>
2020-07-24 13:20:27 +0700

    Added fields and constructor.

--- app/src/main/java/ru/alxabr/newsview/Presenter/MainPresenter.java
+++ app/src/main/java/ru/alxabr/newsview/Presenter/MainPresenter.java
@@ -1,6 +1,10 @@
 package ru.alxabr.newsview.Presenter;
 
-public class MainPresenter {
+import android.content.Context;
+
+import ru.alxabr.newsview.ContractMVP;
+
+public class MainPresenter implements ContractMVP.Presenter {
     private final String[] sources_url = {
             "https://tayga.info/rss",
             "https://nsk.aif.ru/rss/all.php",
@@ -10,4 +14,22 @@ public class MainPresenter {
             "тайга.инфо",
             "Аргументы и Факты",
             "NGS"};
+
+    private ContractMVP.View view;
+    private Context context;
+
+    public MainPresenter(ContractMVP.View view, Context context) {
+        this.view = view;
+        this.context = context;
+    }
+
+    @Override
+    public void showNewsList() {
+
+    }
+
+    @Override
+    public void updateNewsList() {
+
+    }
 }
