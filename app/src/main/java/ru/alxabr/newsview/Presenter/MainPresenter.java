06fc840e0aec252da2be505248b46f0e7744a773
Alexandr Abramenko <alx.abr@bk.ru>
2020-07-24 13:03:52 +0700

    Added urls and names rss lents.

--- app/src/main/java/ru/alxabr/newsview/Presenter/MainPresenter.java
+++ app/src/main/java/ru/alxabr/newsview/Presenter/MainPresenter.java
@@ -1,4 +1,13 @@
 package ru.alxabr.newsview.Presenter;
 
 public class MainPresenter {
+    private final String[] sources_url = {
+            "https://tayga.info/rss",
+            "https://nsk.aif.ru/rss/all.php",
+            "https://news.ngs.ru/rss/"};
+
+    private final String[] sources_name = {
+            "тайга.инфо",
+            "Аргументы и Факты",
+            "NGS"};
 }
